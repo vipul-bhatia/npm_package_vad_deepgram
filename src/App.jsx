2dec46ab082da32a1f6f4254e280790f02fc34ae
Vipul Bhatia <97945914+vipul-bhatia@users.noreply.github.com>
2024-05-26 10:42:46 +0530

    Upgrades

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
 import './App.css';
-import { Quib, Transcriber, useVad } from "@fadedrifleman/web-sdk"
-// import Transcriber from './classes/dgVad';
+import { Quib, Transcriber, useVad } from "@fadedrifleman/web-sdk";
 import { v4 as uuidv4 } from "uuid";
-import { useEffect, useState } from 'react';
-
+import { useEffect, useState, useRef } from 'react';
+import { useQueue } from "@uidotdev/usehooks";
 
 function App() {
+  const transcriberRef = useRef(null);
+  const [buttonClicked, setButtonClicked] = useState(false);
+  const { add, remove, first, size, queue, clear } = useQueue([]);
+  const [uMedia, setUMedia] = useState(null);
 
-
-
-
-  // const { add, remove, first, size, queue, clear } = useQueue([]);
-
-  //const quib = new Quib({ host: 'quibbleai.io:8888', uid: uuidv4() });
-  //const transcriber = new Transcriber({ apiKey: '656759acdbd9f56cf70b91f71f38cd2eddacf5c4' });
-  const [uMedia, setUMedia] = useState(null)
-
-
-
+  useEffect(() => {
+    const transcriber = new Transcriber({ apiKey: '' });
+    transcriber.on('transcription', (text) => {
+      console.log(text);
+    });
+    transcriberRef.current = transcriber;
+  }, []);
 
   const { start, pause } = useVad({
     userMedia: uMedia,
@@ -29,29 +28,23 @@ function App() {
     },
   });
 
+  useEffect(() => {
+    if (buttonClicked) {
+      const quib = new Quib({ host: 'quibbleai.io:8888', uid: uuidv4() });
 
+      quib.on('connected', () => {
+        console.log('Connected to Quib');
+        quib.start();
+        quib.send('hello');
+      });
 
-  // transcriber.on('transcription', (text) => {
-  //   console.log(text)
-  // })
-  // quib.on('connected', () => {
-  //   console.log('Connected to Quib')
-  //   quib.start()
-  //   quib.send('hello')
-  // })
-
-  // quib.on('media', (data) => {
-  //   console.log(data)
-  // })
-
-
-
-
+      quib.on('media', (data) => {
+        console.log(data);
+      });
+    }
+  }, [buttonClicked]);
 
   async function microphone() {
-
-
-
     const userMedia = await navigator.mediaDevices.getUserMedia({
       audio: {
         noiseSuppression: true,
@@ -59,42 +52,27 @@ function App() {
       },
     });
 
-
-
     const microphone = new MediaRecorder(userMedia);
-    setUMedia(userMedia)
-
-    start()
-
-
-
+    setUMedia(userMedia);
 
     microphone.start(500);
+
     microphone.onstart = () => {
       console.log("Now Call Started.");
+      setButtonClicked(true);
     };
     microphone.ondataavailable = (e) => {
-      // add(e.data);
-      console.log(e.data)
-      //  transcriber.send(e.data)
+      console.log(e.data);
+      console.log('dataa');
 
-      // remove()
-
+      transcriberRef.current.send(e.data);
     };
-
   }
-
-  // useEffect(() => {
-
-  // }, [queue])
-
 
   return (
     <div className="App">
       <header className="App-header">
-
         <button onClick={microphone}>Start Call</button>
-
       </header>
     </div>
   );
--- src/classes/vad.jsx
+++ src/classes/vad.jsx
@@ -1,31 +1,125 @@
-import { useEffect, useRef } from "react";
-import { useMicVAD } from "@ricky0123/vad-react";
+// version of deepgram - 3.2.0
+import { EventEmitter } from "events";
+import { LiveTranscriptionEvents, createClient } from "@deepgram/sdk";
 
-const useVad = (config) => {
-    const { userMedia, onSpeechStart, onSpeechEnd } = config;
-    const vadRef = useRef();
+class Transcriber extends EventEmitter {
+    constructor({ apiKey }) {
+        super();
+        this.deepgramApiKey = apiKey;
+        this.finalResult = "";
+        this.speechFinal = false;
+        this.timeOutCheck = null;
 
-    vadRef.current = useMicVAD({
-        startOnLoad: true,
-        stream: userMedia || undefined,
-        onSpeechStart: () => onSpeechStart(),
-        onSpeechEnd: () => onSpeechEnd(),
-        positiveSpeechThreshold: 0.6,
-        negativeSpeechThreshold: 0.6 - 0.45,
-    });
+        this.initDeepgram();
+    }
 
+    initDeepgram() {
+        this.deepgram = createClient(this.deepgramApiKey);
+        this.deepgramLive = this.deepgram.listen.live({
+            model: "nova-2-general",
+            language: "en-IN",
+            interim_results: true,
+            filler_words: true,
+            smart_format: true,
+            endpointing: 450,
+            utterance_end_ms: 1000,
+            utterances: true,
+            vad_events: true,
+        });
 
+        this.setEventListeners();
+    }
 
+    setEventListeners() {
 
-    const start = () => {
-        vadRef.current?.start();
-    };
+        this.deepgramLive.on(LiveTranscriptionEvents.Open, () => {
 
-    const pause = () => {
-        vadRef.current?.pause();
-    };
+            console.log("Deepgram Connection Established");
+            // this.emit("connected", "deegpram connected");
+        });
 
-    return { start, pause };
-};
+        this.deepgramLive.on(LiveTranscriptionEvents.Error, (error) =>
+            console.error(error)
+        );
 
-export default useVad;
+        this.deepgramLive.on(LiveTranscriptionEvents.Close, () =>
+            console.log("connection closed")
+        );
+
+        this.deepgramLive.on(
+
+            LiveTranscriptionEvents.Transcript,
+            this.handleTranscript.bind(this)
+        );
+    }
+
+    handleTranscript(transcription) {
+        const alternatives = transcription.channel?.alternatives;
+        const text = alternatives ? alternatives[0]?.transcript : "";
+
+        if (text) {
+            console.log(transcription);
+            this.emit("partialTranscription", text);
+            this.clearTimeoutCheck();
+        }
+
+        if (text.length > 0 && transcription.is_final === true) {
+            this.finalResult += `${text} `;
+        }
+
+        if (transcription.is_final === true) {
+            this.setTimeoutCheck();
+
+            if (text.length > 0 && transcription.speech_final === true) {
+                this.clearTimeoutCheck();
+                this.emitTranscription();
+                this.speechFinal = true;
+            } else {
+                this.speechFinal = false;
+            }
+        }
+    }
+
+    setTimeoutCheck() {
+        this.timeOutCheck = setTimeout(() => {
+            console.log("timeout triggered");
+            if (this.finalResult.length > 1) {
+                this.emitTranscription();
+                console.log("failsafe triggered, speechFinal not received");
+            }
+        }, 1500);
+    }
+
+    clearTimeoutCheck() {
+        if (this.timeOutCheck) {
+            clearTimeout(this.timeOutCheck);
+            this.timeOutCheck = null;
+        }
+    }
+
+    emitTranscription() {
+        this.emit("transcription", this.finalResult);
+        this.finalResult = "";
+    }
+
+    send(data) {
+        if (this.deepgramLive.getReadyState() === 1) {
+
+            this.deepgramLive.send(data);
+            console.log(data, 'transcriptions')
+            console.log('dataaaa')
+        }
+    }
+
+    keepAlive() {
+        if (this.deepgramLive.getReadyState() === 1) {
+            this.deepgramLive.send(JSON.stringify({ type: "KeepAlive" }));
+        }
+    }
+
+    close() {
+        this.deepgramLive.send(JSON.stringify({ type: "CloseStream" }));
+    }
+}
+
+export default Transcriber;
